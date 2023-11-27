94af3e273e1846403cc86cf02efbc31147ce766d
maxDes23 <133263504+maxDes23@users.noreply.github.com>
2023-11-27 17:33:01 +0200

    friends

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import user from '../data/user.json';
 import data from '../data/data.json';
+import friends from '../data/friends.json';
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
+import FriendList from './Friendlist/Friendlist';
 
 export const App = () => {
   return (
@@ -13,10 +15,10 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics title="Upload stats" stats={data} />
+      <Statistics title="Upload stats" stats={data} />;
+      <FriendList friends={friends} />;
 
-      {/* <FriendList friends={friends} />
-      <TransactionHistory items={transactions} /> */}
+      {/* <TransactionHistory items={transactions} /> */}
     </div>
   );
 };
---
+++ src/components/Friendlist/Friendlist.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Friendlist.module.css';
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className={styles.item}>
+      <span className={isOnline ? styles.online : styles.offline}></span>
+      <img
+        className={styles.avatar}
+        src={avatar}
+        alt={name}
+        width="48"
+      />
+      <p className={styles.name}>{name}</p>
+    </li>
+  );
+};
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={styles.list}>
+      {friends.map(friend => (
+        <FriendListItem
+          key={friend.id}
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+};
+
+export default FriendList;
